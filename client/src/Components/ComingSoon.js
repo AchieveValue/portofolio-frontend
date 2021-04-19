39ec7b1c331d959af8701aacfd13b8b47301fdf1
Andrei Bunea <buneaandrei24@gmail.com>
2021-04-19 17:30:04 +0300

    The last animation fix for today..

--- client/src/Components/ComingSoon.js
+++ client/src/Components/ComingSoon.js
@@ -9,20 +9,22 @@ import parent from './Animations';
 
 const ComingSoon = () => {
     return (
-        <motion.div variants={parent} initial="hidden" animate="show" exit="exit">
+        <>
             <NavBar dark={true}/>
-            <motion.div>
-                <div className="container-fluid" style={{ background: '#222335', paddingTop: '15vh', paddingBottom: '15vh' }}>
-                    <div className="container d-flex align-items-center justify-content-center text-light flex-column flex-lg-row text-center">
-                        <motion.h1 className="mr-5 display-4" initial={{ rotateY: 0 }} animate={{ rotateY: [360, 0] }}><i class="fas fa-hourglass-half"></i></motion.h1>
-                        <div className="">
-                            <h2 className="rightenous">This page is not ready yet.</h2>
-                            <h5 className="poppins font-weight-light mt-4">We assure you that we are doing our best to finish it! You may want to go to <Link to="/" className="text-light font-weight-bold">home page</Link>.</h5>
+            <motion.div variants={parent} initial="hidden" animate="show" exit="exit">
+                <motion.div>
+                    <div className="container-fluid" style={{ background: '#222335', paddingTop: '15vh', paddingBottom: '15vh' }}>
+                        <div className="container d-flex align-items-center justify-content-center text-light flex-column flex-lg-row text-center">
+                            <h1 className="mr-5 display-4"><i class="fas fa-hourglass-half"></i></h1>
+                            <div className="">
+                                <h2 className="rightenous">This page is not ready yet.</h2>
+                                <h5 className="poppins font-weight-light mt-4">We assure you that we are doing our best to finish it! You may want to go to <Link to="/" className="text-light font-weight-bold">home page</Link>.</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </motion.div>
-        </motion.div>
+        </>
     )
 }
 
